596fae4aeec353fb4614ac9ac725ffd63c704980
Nain Lobato <wolverinenain@gmail.com>
2018-03-08 07:52:18 -0600

    Tarea realizada: Actualización de la tabla cat_estado y cat_muncipio.

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -8,7 +8,6 @@
 	<link rel="icon" href="{{ asset('img/iconofge.png') }}">
 	<!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
 	{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">--}}
@@ -29,11 +28,17 @@
 		@yield('content')
 	</div>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>--}}
+    <script src="{{ asset('js/jquery-3.2.1.min.js')}}" ></script>
+    <script src="{{ asset('js/popper.js')}}" ></script>
+	<script src="{{ asset('js/bootstrap.js')}}" ></script>
+	<script src="{{asset ('js/sweetalert.min.js')}}"></script>
 	<script src="{{ asset('plugins/select2/select2.min.js')}}" ></script>
+    <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
 	<script type="text/javascript">
 		$("input:text").keyup(function() {
         $(this).val($(this).val().toUpperCase());
@@ -45,14 +50,6 @@
             $('select').select2();
         });
     </script>
-	{{--Por si no hay internet
-    <script src="{{ asset('js/jquery-3.2.1.min.js')}}" ></script>
-    <script src="{{ asset('js/popper.js')}}" ></script>
-	<script src="{{ asset('js/bootstrap.js')}}" ></script>
-	<script src="{{asset ('js/sweetalert.min.js')}}"></script>
-	xxxxxxxxxxxxxxxxxxxx --}}
-    <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
 	@yield('scripts')
     @include('sweet::alert')
 </body>
