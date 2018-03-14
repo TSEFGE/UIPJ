9517b855d847df74fee2c24677e4df3c5bb3c6fd
Donaldo Lopez <donaldolog@gmail.com>
2018-03-14 09:22:41 -0600

    Tarea: Consulta de libro con rango de fechas.

--- app/Http/Controllers/LibroGobiernoController.php
+++ app/Http/Controllers/LibroGobiernoController.php
@@ -27,4 +27,21 @@ class LibroGobiernoController extends Controller
             ->get();
       return Datatables::of($libro)->make(true);
     }
+    
+    public function apiLibroRango(Request $request){
+      $libro = DB::table('carpeta')
+            ->join('users','users.id','=','carpeta.idFiscal')
+            ->join('unidad','unidad.id','=','users.idUnidad')
+            ->join('variables_persona','variables_persona.idCarpeta','=','carpeta.id')
+            ->join('extra_denunciante','extra_denunciante.idVariablesPersona','=','variables_persona.id')
+            ->join('persona','persona.id','=','variables_persona.idPersona')
+            ->select('carpeta.fechaInicio as Fecha','carpeta.numCarpeta','unidad.nombre as Unidad',DB::raw('CONCAT(users.nombres, " ", users.primerAp," ", ifnull(users.segundoAp,"")) AS Fiscal'),DB::raw('group_concat(persona.nombres," ",ifnull(persona.primerAp,"")," ",ifnull(persona.segundoAp,"")) as Denunciante'),'carpeta.descripcionHechos as Nota')
+            ->groupBy('carpeta.id')
+            ->where('unidad.id','=', Auth::user()->idUnidad)
+            ->whereDate('carpeta.created_at', '>=',$request->fechaInicial)
+            ->whereDate('carpeta.created_at', '<=',$request->fechaFinal)
+            ->get();
+      return Datatables::of($libro)->make(true);
+    }
+
 }
