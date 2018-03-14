66ea6c8661012c40e3a069525bb644437b27d1a6
Nain Lobato <wolverinenain@gmail.com>
2018-03-14 09:32:11 -0600

    Corrección input fechas.

--- resources/views/librogobierno.blade.php
+++ resources/views/librogobierno.blade.php
@@ -14,9 +14,9 @@
 			<div class="row">
 				<div class="col-5" align="center">
 					<div class="form-group">
-						{!! Form::label('fechaLibroIni', 'De Fecha:', ['class' => 'col-form-label-sm']) !!}
+						{!! Form::label('fechaIni', 'De Fecha:', ['class' => 'col-form-label-sm']) !!}
 						<div class="input-group date" id="fechaLibroIni" data-target-input="nearest">
-							{!! Form::text('fechaLibroIni', null, ['class' => 'form-control form-control-sm datetimepicker-input', 'data-target' => '#fechaLibroIni', 'data-toggle' => 'datetimepicker', 'required', 'placeholder' => 'DD/MM/AAAA']) !!}
+							{!! Form::text('fechaIni', null, ['class' => 'form-control form-control-sm datetimepicker-input', 'data-target' => '#fechaLibroIni', 'data-toggle' => 'datetimepicker', 'required', 'placeholder' => 'DD/MM/AAAA']) !!}
 							<div class="input-group-append" data-target="#fechaLibroIni" data-toggle="datetimepicker">
 								<div class="input-group-text"><i class="fa fa-calendar"></i></div>
 							</div>
@@ -26,9 +26,9 @@
 
 				<div class="col-5" align="center" >
 					<div class="form-group">
-						{!! Form::label('fechaLibroFin', 'A Fecha:', ['class' => 'col-form-label-sm']) !!}
+						{!! Form::label('fechaFin', 'A Fecha:', ['class' => 'col-form-label-sm']) !!}
 						<div class="input-group date" id="fechaLibroFin" data-target-input="nearest">
-							{!! Form::text('fechaLibroFin', null, ['class' => 'form-control form-control-sm datetimepicker-input', 'data-target' => '#fechaLibroFin', 'data-toggle' => 'datetimepicker', 'required', 'placeholder' => 'DD/MM/AAAA']) !!}
+							{!! Form::text('fechaFin', null, ['class' => 'form-control form-control-sm datetimepicker-input', 'data-target' => '#fechaLibroFin', 'data-toggle' => 'datetimepicker', 'required', 'placeholder' => 'DD/MM/AAAA']) !!}
 							<div class="input-group-append" data-target="#fechaLibroFin" data-toggle="datetimepicker">
 								<div class="input-group-text"><i class="fa fa-calendar"></i></div>
 							</div>
@@ -120,8 +120,8 @@
     });
     $('#consultar').on('click',function(e){
         var route="{{route('api.libro.rango')}}";
-        var fechaIni=$("#fechaLibroIni").val();
-        var fechaFin=$("#fechaLibroFin").val();
+        var fechaIni=$("#fechaIni").val();
+        var fechaFin=$("#fechaFin").val();
         if(fechaIni!="" || fechaFin!=""){
             $.ajax({
                 type: 'POST',
