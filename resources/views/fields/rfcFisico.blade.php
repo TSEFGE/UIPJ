c604a9b35191775deb10aceb1928079d0a124143
Nain Lobato <wolverinenain@gmail.com>
2018-03-21 09:34:38 -0600

    Mensaje Toast al actualizar el RFC y el CURP.

--- resources/views/fields/rfcFisico.blade.php
+++ resources/views/fields/rfcFisico.blade.php
@@ -1,75 +1,76 @@
 <script>
-  $("#nombres").focusout(function() {
-    obtenerRFCFISICA();
-  });
-  $("#primerAp").focusout(function() {
-    obtenerRFCFISICA();
-  });
-  $("#segundoAp").focusout(function() {
-    obtenerRFCFISICA();
-  });
-  $("#fechaNacimiento").focusout(function() {
-    obtenerRFCFISICA();
-  });
+	$("#nombres").focusout(function() {
+		obtenerRFCFISICA();
+	});
+	$("#primerAp").focusout(function() {
+		obtenerRFCFISICA();
+	});
+	$("#segundoAp").focusout(function() {
+		obtenerRFCFISICA();
+	});
+	$("#fechaNacimiento").focusout(function() {
+		obtenerRFCFISICA();
+	});
 
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-  });
+	$.ajaxSetup({
+		headers: {
+			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+		}
+	});
 
-  function obtenerRFCFISICA() {
-    nombre = $("#nombres").val().toUpperCase();
-    apPaterno = $("#primerAp").val().toUpperCase();
-    apMaterno = $("#segundoAp").val().toUpperCase();
-    fecha = $("#fechaNacimiento").val();
+	function obtenerRFCFISICA() {
+		nombre = $("#nombres").val().toUpperCase();
+		apPaterno = $("#primerAp").val().toUpperCase();
+		apMaterno = $("#segundoAp").val().toUpperCase();
+		fecha = $("#fechaNacimiento").val();
 
-    console.log(fecha);
+		console.log(fecha);
 
-    dia = fecha.substr(8, 2);
-    mes = fecha.substr(5, 2);
-    año = fecha.substr(2, 2);
+		dia = fecha.substr(8, 2);
+		mes = fecha.substr(5, 2);
+		año = fecha.substr(2, 2);
 
-    ruta = "{{route('rfcFisico.denunciante')}}";
-    $.ajax({
-      type: "POST",
-      url: ruta,
-      data: {
-        nombre,
-        apPaterno,
-        apMaterno,
-        dia,
-        mes,
-        año
-      },
-      dataType: "json",
-      success: function(data) {
+		ruta = "{{route('rfcFisico.denunciante')}}";
+		$.ajax({
+			type: "POST",
+			url: ruta,
+			data: {
+				nombre,
+				apPaterno,
+				apMaterno,
+				dia,
+				mes,
+				año
+			},
+			dataType: "json",
+			success: function(data) {
 
-        rfc=data.res;
-        var array= rfc.split("");
-        var rfcSH=[];
-        var homoC=[];
+				rfc=data.res;
+				var array= rfc.split("");
+				var rfcSH=[];
+				var homoC=[];
 
-        array.forEach( function(valor, indice, array) { 
+				array.forEach( function(valor, indice, array) { 
 
-          if(indice<10){
-            rfcSH.push(valor);
-          } if(indice>=10){
-            homoC.push(valor); 
-          } 
-        });
+					if(indice<10){
+						rfcSH.push(valor);
+					} if(indice>=10){
+						homoC.push(valor); 
+					} 
+				});
 
-        rfc=rfcSH.join("");;
-        homo=homoC.join("");
+				rfc=rfcSH.join("");;
+				homo=homoC.join("");
 
-        $("#rfc").val(rfc);
+				$("#rfc").val(rfc);
 
-        $("#homo").val(homo);
+				$("#homo").val(homo);
 
-      },
-      error: function(data) {
-        // console.log(data);
-      }
-    });
-  }
+			},
+			error: function(data) {
+				// console.log(data);
+			}
+		});
+		toastr.info('Se ha modificado el RFC', '¡Atención!');
+	}
 </script>
--- resources/views/fields/rfcMoral.blade.php
+++ resources/views/fields/rfcMoral.blade.php
@@ -1,62 +1,61 @@
 <script>
 	$("#nombres2").focusout(function() {
-  obtenerRFC();
+	obtenerRFC();
 });
 
 $("#fechaAltaEmpresa").focusout(function() {
-  obtenerRFC();
+	obtenerRFC();
 });
 
 $.ajaxSetup({
-  headers: {
-    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-  }
+	headers: {
+		'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+	}
 });
 
 function obtenerRFC(){
 
-console.log('entra ajax');
+	console.log('entra ajax');
 
-nombre = $("#nombres2").val().toUpperCase();
-fecha = $("#fechaAltaEmpresa").val();
-	
+	nombre = $("#nombres2").val().toUpperCase();
+	fecha = $("#fechaAltaEmpresa").val();
+		
+	console.log(fecha);
 
-console.log(fecha);
+		dia = fecha.substr(8, 2);
+		mes = fecha.substr(5, 2);
+		ano = fecha.substr(2, 2);
 
-  dia = fecha.substr(8, 2);
-  mes = fecha.substr(5, 2);
-  ano = fecha.substr(2, 2);
+	ruta="{{route('rfc.denunciante')}}";
+	$.ajax({
+		type: "POST",
+		url:ruta,
+		data: {nombre,dia,mes,ano},
+		dataType:"json",
+		success: function(data){
+				
+			rfc=data.res;
+			var array= rfc.split("");
+			var rfcSH=[];
+			var homoC=[];
 
-ruta="{{route('rfc.denunciante')}}";
-$.ajax({
-      type: "POST",
-      url:ruta,
-      data: {nombre,dia,mes,ano},
-      dataType:"json",
-     success: function(data){
-        
-        rfc=data.res;
-        var array= rfc.split("");
-        var rfcSH=[];
-        var homoC=[];
+			array.forEach( function(valor, indice, array) { 
+				if(indice<9){
+					rfcSH.push(valor);
+				} if(indice>=9){
+					homoC.push(valor); 
+				} 
+			});
 
-        array.forEach( function(valor, indice, array) { 
+			rfc=rfcSH.join("");
+			homo=homoC.join("");
 
-          if(indice<9){
-            rfcSH.push(valor);
-          } if(indice>=9){
-            homoC.push(valor); 
-          } 
-        });
-
-        rfc=rfcSH.join("");
-        homo=homoC.join("");
-
-        $("#rfc2").val(rfc);
-        $("#homo2").val(homo);
-   },error:function(data){
-   // console.log(data);
-   }
- });
+			$("#rfc2").val(rfc);
+			$("#homo2").val(homo);
+	 	},error:function(data){
+	 		// console.log(data);
+	 	}
+ 	});
+	toastr.info('Se ha modificado el RFC', '¡Atención!');
 } 
 </script>
