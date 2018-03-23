83e0d846fb05b112440ca334daedcc35e678b249
ADFGE\kguzman <isckg93@gmail.com>
2018-03-23 13:10:29 -0600

    Calendar

---
+++ resources/views/fields/ajaxCurp.blade.php
@@ -0,0 +1,21 @@
+@push('PilaScripts')
+<script type="text/javascript">
+$("#curp").on("change focusout", function() {
+	var curp=$(this).val();
+	$.get("/persona/curp/"+curp, function(response, estado){
+		if(response.res==true){
+				console.log('true')
+				swal({
+				title: "Alerta",
+				text: "Ya existe una persona registrada con ese CURP.",
+				type: "warning",
+				showCancelButton: false,
+				confirmButtonClass: "btn-danger",
+				confirmButtonText: "Aceptar",
+				closeOnConfirm: false
+				});
+				}
+	});
+});
+</script>
+@endpush
