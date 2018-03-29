a374a3130a0754ab4d7149deb05cb92cc6ab0d67
Donaldo Lopez <donaldolog@gmail.com>
2018-03-29 12:55:38 -0600

    Registra en bitacora inicio y cierre de sesión de usuario.

---
+++ app/Listeners/LogSuccessfulLogout.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Logout;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Bitacora;
+
+class LogSuccessfulLogout
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Logout  $event
+     * @return void
+     */
+    public function handle(Logout $event)
+    {
+         Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'users', 'accion' => 'Logout', 'descripcion' => 'Cierre de sesión.', 'idFilaAccion' => 0]);
+    }
+}
