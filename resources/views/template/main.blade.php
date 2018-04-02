c07cfde090861e867e4c0989b13a5d075c86e657
Donaldo Lopez <donaldolog@gmail.com>
2018-04-02 09:08:35 -0500

    Correccion;

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -9,7 +9,7 @@
 	<link rel="icon" href="{{ asset('img/iconofge.png') }}">
 	<!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-		@include('template.notAllowedScript');
+		@include('template.notAllowedScript')
 	{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">--}}
