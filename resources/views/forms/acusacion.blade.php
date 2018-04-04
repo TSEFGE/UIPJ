9bdd3a53210f826c46aed6c2179cba544a661b63
ADFGE\kguzman <isckg93@gmail.com>
2018-04-04 10:47:49 -0500

    Rediseño de formulario de acusación

--- resources/views/forms/acusacion.blade.php
+++ resources/views/forms/acusacion.blade.php
@@ -1,19 +1,41 @@
 @extends('template.form')
 
 @section('title', 'Agregar Acusación')
+
+@section('header')
+{!! Form::open(['route' => 'store.acusacion', 'method' => 'POST'])  !!}
+{{ csrf_field() }}
+<div class="row">
+		<div class="col">
+			<div class="text-left">
+				{{--Aqui van radios, etc --}}
+			</div>
+		</div>
+		<div class="col">	
+			<div class="text-right">
+				@include('forms.buttons')
+			</div>
+		</div>
+	</div>
+@endsection
+
 @section('contenido')
-    {!! Form::open(['route' => 'store.acusacion', 'method' => 'POST'])  !!}
-    {{ csrf_field() }}
+@include('forms.errores')   
 	<div class="row no-gutters">
 		<div class="col-12">
 			<div class="boxtwo">
+				<h6>Datos de la acusación</h6>
 				@include('fields.acusacion')
 			</div>
 		</div>
 	</div>
-	@include('forms.buttons')
+	
 	{!! Form::close() !!}
+@endsection
+
+@section('tabla')
 	<div class="boxtwo">
+		@section('titulo-tabla','Acusaciones registradas')
 		@include('tables.acusaciones')
 	</div>
 @endsection
