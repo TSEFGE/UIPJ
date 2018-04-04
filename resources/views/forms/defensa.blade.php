9c8898785d9c215757d17594e59deb166044d558
Nain Lobato <wolverinenain@gmail.com>
2018-04-04 10:27:03 -0500

    Ajuste de formulario de Defensa.

--- resources/views/forms/defensa.blade.php
+++ resources/views/forms/defensa.blade.php
@@ -1,20 +1,41 @@
 @extends('template.form')
 
-@section('title', 'Agregar Defensa')
-@section('contenido')
-    {!! Form::open(['route' => 'store.defensa', 'method' => 'POST'])  !!}
-    {{ csrf_field() }}
-	@include('forms.idcarpeta')
-	<div class="row no-gutters">
-		<div class="col-12">
-			<div class="boxtwo">
-				@include('fields.defensa')
+@section('title', 'Agregar defensa')
+
+@section('header')
+{!! Form::open(['route' => 'store.defensa', 'method' => 'POST'])  !!}
+{{ csrf_field() }}
+<div class="row">
+		<div class="col">
+			<div class="text-left">
+				{{--Aqui van radios, etc --}}
+			</div>
+		</div>
+		<div class="col">	
+			<div class="text-right">
+				@include('forms.buttons')
 			</div>
 		</div>
 	</div>
-	@include('forms.buttons')
+@endsection
+
+@section('contenido')
+	@include('forms.errores')
+	<div class="row no-gutters">
+		<div class="col-12">
+			<div class="boxtwo">
+				<div class="row">
+					@include('fields.defensa')
+				</div>
+			</div>
+		</div>
+	</div>
 	{!! Form::close() !!}
+@endsection
+
+@section('tabla')
 	<div class="boxtwo">
+		@section('titulo-tabla', 'Defensas registradas')
 		@include('tables.defensas')
 	</div>
 @endsection
