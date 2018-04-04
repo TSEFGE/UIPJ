03e81ac298a6bb5d0b7a5b9ece9330fff427a219
ADFGE\garenas <uncubus.agaa@gmail.com>
2018-04-04 13:48:21 -0500

    Modficiacion familiares

--- resources/views/template/form.blade.php
+++ resources/views/template/form.blade.php
@@ -7,8 +7,7 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
-        <div class="card">
-            
+        <div class="card">            
                 @yield('contenido')
             </div>
         </div>
