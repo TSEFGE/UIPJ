6978876110a23198850c21b421e611e89a979a8c
Nain Lobato <wolverinenain@gmail.com>
2018-04-05 11:47:57 -0500

    Active Directory Completo :D

--- resources/views/template/partials/sidebar.blade.php
+++ resources/views/template/partials/sidebar.blade.php
@@ -15,7 +15,7 @@
 				<img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
 			</div>
 			<div class="info">
-				<a href="#" class="d-block">{{ Auth::user()->nombres." ". Auth::user()->primerAp." ".Auth::user()->segundoAp }}</a>
+				<a href="#" class="d-block">{{ Auth::user()->nombres." ". Auth::user()->apellidos }}</a>
 				{{--<a href="#" class="d-block">{{ Auth::user()->nombres." ".Auth::user()->primerAp }}</a>--}}
 				<a href="#" class="d-block"> <small>Numero de fiscal </small><b> {{ Auth::user()->numFiscal}}</b></a>
 			</div>
