a29d705000a4c92712b240e88172372f6eb1b1fb
Nain Lobato <wolverinenain@gmail.com>
2018-04-06 08:48:29 -0500

    Se elimina checkbox de viíctima por radiobuttons.

--- resources/views/fields/tipo-persona.blade.php
+++ resources/views/fields/tipo-persona.blade.php
@@ -1,25 +1,14 @@
-<div class="col-md-3">
-	<div class="form-group">
-		<label class="col-form-label col-form-label-sm" for="formGroupExampleInput">¿Es Persona Moral?</label>
-		<div class="clearfix"></div>
-		<div class="form-check form-check-inline">
-			<label class="form-check-label col-form-label col-form-label-sm">
-				<input class="form-check-input" type="radio" id="esEmpresa1" name="esEmpresa" value="1" required> Sí
-			</label>
-		</div>
-		<div class="form-check form-check-inline">
-			<label class="form-check-label col-form-label col-form-label-sm">
-				<input class="form-check-input" type="radio" id="esEmpresa2" name="esEmpresa" value="0" required> No
-			</label>
-		</div>
-	</div>
-
-</div>
-<div class="form-group" id='Victima'>
-	<br>
+<div class="form-group">
+	<label class="col-form-label col-form-label-sm" for="formGroupExampleInput">¿Es persona moral?</label>
+	<div class="clearfix"></div>
 	<div class="form-check form-check-inline">
 		<label class="form-check-label col-form-label col-form-label-sm">
-		<input class="form-check-input" type="checkbox" id="esVictima" name="esVictima" value="1">¿Es Victima?
-	</label>
+			<input class="form-check-input" type="radio" id="esEmpresa1" name="esEmpresa" value="1" required> Sí
+		</label>
+	</div>
+	<div class="form-check form-check-inline">
+		<label class="form-check-label col-form-label col-form-label-sm">
+			<input class="form-check-input" type="radio" id="esEmpresa2" name="esEmpresa" value="0" required> No
+		</label>
 	</div>
 </div>
