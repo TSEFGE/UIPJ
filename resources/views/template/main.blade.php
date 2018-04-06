c7ccbfa26f060ca008c22a4348a76b9c45def971
Nain Lobato <wolverinenain@gmail.com>
2018-04-06 11:58:48 -0500

    Funcionalidad de inactividad de sesión aplicada.

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -9,7 +9,7 @@
 	<link rel="icon" href="{{ asset('img/iconofge.png') }}">
 	<!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-		@include('template.notAllowedScript')
+	@include('template.notAllowedScript')
 	{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">--}}
@@ -117,6 +117,9 @@
 
 	<!-- PAGE SCRIPTS -->
 	<script src="{{ asset('admin/dist/js/pages/dashboard2.js') }}"></script>
+
+	<script type="text/javascript" src="{{ asset('js/idle-timer.min.js') }}"></script>
+	@include('template.scriptExpireSession')
 
 	<script type="text/javascript">
 
