2b1d9c7f97b4bcb63e54cc177f2aa0ed0c1a8687
ADFGE\garenas <uncubus.agaa@gmail.com>
2018-04-11 15:01:47 -0500

    Modificacion a span de notificaciones

--- resources/views/forms/denunciante.blade.php
+++ resources/views/forms/denunciante.blade.php
@@ -50,7 +50,7 @@
 
 						<ul class="nav nav-tabs">
 							<li class="nav-item" id="datosPer">
-								<a class="nav-link active pestaña" id="personal" data-toggle="tab" href="#collapsePersonales1"><i class="fa fa-user-circle-o" aria-hidden="true"></i></a>
+								<a class="nav-link active "  data-toggle="tab" href="#collapsePersonales1"><p id="personal" class="pestaña"><i class="fa fa-user-circle-o" aria-hidden="true"></p></i>
 								<span id="tab1" class="xvacio"></span>
 								<span id="txtTab1" class="error"></span>
 								<span id="t1" class="bien"></span>
@@ -58,24 +58,24 @@
 								</a>
 							</li>
 							<li class="nav-item" id="datosDir">
-								<a class="nav-link" data-toggle="tab" id="direccion" href="#collapseDir1"><i class="fa fa-address-card" aria-hidden="true"></i></a>
-								<span id="tab2"></span>
+								<a class="nav-link" data-toggle="tab"  href="#collapseDir1"><p id="direccion" class="pestaña"><i class="fa fa-address-card" aria-hidden="true"></i></p>
+								<span id="tab2" class="xvacio"></span>
 								<span id="txtTab2" class="error"></span>
 								<span id="t2" class="bien"></span>
 								
 								</a>
 							</li>
 							<li class="nav-item" id="datosTrab">
-								<a class="nav-link" data-toggle="tab" id="dtrabajo" href="#collapseTrab1"><i class="fa fa-industry" aria-hidden="true"></i></a>
-								<span id="tab3"></span>
+								<a class="nav-link" data-toggle="tab"  href="#collapseTrab1"><p id="dtrabajo" class="pestaña"> <i class="fa fa-industry" aria-hidden="true"></i></p>
+								<span id="tab3" class="xvacio"></span>
 								<span id="txtTab3" class="error"></span>
 								<span id="t3" class="bien"></span>
 								
 								</a>
 							</li>
 							<li class="nav-item" id="datosNotif">
-								<a class="nav-link" data-toggle="tab" id="dnotificaciones" href="#collapseNotifs1"><i class="fa fa-bell" aria-hidden="true"></i></a>
-									<span id="tab4"></span>
+								<a class="nav-link" data-toggle="tab"  href="#collapseNotifs1"><p id="dnotificaciones" class="pestaña"><i class="fa fa-bell" aria-hidden="true"></i></p>
+									<span id="tab4" class="xvacio"></span>
 									<span id="txtTab4" class="error"></span>
 									<span id="t4" class="bien"></span>
 								</a>
