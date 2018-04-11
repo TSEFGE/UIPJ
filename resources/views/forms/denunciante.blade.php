cc6e02a2f332292225a7679b1b05853d1a17c5bd
ADFGE\afernandezl <ing.a.landero@gmail.com>
2018-04-11 14:12:24 -0500

    Modificacion notificacion tabs

--- resources/views/forms/denunciante.blade.php
+++ resources/views/forms/denunciante.blade.php
@@ -50,16 +50,41 @@
 
 						<ul class="nav nav-tabs">
 							<li class="nav-item" id="datosPer">
-								<a class="nav-link active pestaña" id="personal" data-toggle="tab" href="#collapsePersonales1"><i class="fa fa-user-circle-o" aria-hidden="true"></i></a>
+								<a class="nav-link active" data-toggle="tab" href="#collapsePersonales1">Datos personales							
+								<span id="tab1" class="xvacio"></span>
+								<span id="txtTab1" class="error"></span>
+								<span id="t1" class="bien"></span>
+								
+								</a>
 							</li>
 							<li class="nav-item" id="datosDir">
-								<a class="nav-link" data-toggle="tab" id="direccion" href="#collapseDir1"><i class="fa fa-address-card" aria-hidden="true"></i></a>
+								<a class="nav-link" data-toggle="tab" href="#collapseDir1">Dirección
+								<span id="tab2"></span>
+								<span id="txtTab2" class="error"></span>
+								<span id="t2" class="bien"></span>
+								
+								</a>
 							</li>
 							<li class="nav-item" id="datosTrab">
-								<a class="nav-link" data-toggle="tab" id="dtrabajo" href="#collapseTrab1"><i class="fa fa-industry" aria-hidden="true"></i></a>
+								<a class="nav-link" data-toggle="tab" href="#collapseTrab1">Datos del trabajo
+								<span id="tab3"></span>
+								<span id="txtTab3" class="error"></span>
+								<span id="t3" class="bien"></span>
+								
+								</a>
 							</li>
 							<li class="nav-item" id="datosNotif">
-								<a class="nav-link" data-toggle="tab" id="dnotificaciones" href="#collapseNotifs1"><i class="fa fa-bell" aria-hidden="true"></i></a>
+								<a class="nav-link" data-toggle="tab" href="#collapseNotifs1">Dirección para notificaciones
+									<span id="tab4"></span>
+									<span id="txtTab4" class="error"></span>
+									<span id="t4" class="bien"></span>
+								</a>
+							</li>
+							<li class="nav-item" id="datosExtra">
+								<a class="nav-link" data-toggle="tab" href="#collapseDenun1">Información sobre el denunciante o agraviado
+									
+									
+								</a>
 							</li>
 
 						</ul>
@@ -69,7 +94,6 @@
 					<div class="tab-content" id="ctdenunciante">
 						<div class="tab-pane active container" id="collapsePersonales1">
 							@include('fields.personales')
-							@include('fields.extra-denunciante')
 
 						</div>
 						<div class="tab-pane container" id="collapseDir1">
@@ -80,7 +104,11 @@
 						</div>
 						<div class="tab-pane container" id="collapseNotifs1">
 							@include('fields.notificaciones')
-						</div>						
+						</div>
+						<div class="tab-pane container" id="collapseDenun1">
+							@include('fields.extra-denunciante')
+
+						</div>
 					</div>
 				</div>
 					<!-- Fin pestañas -->
@@ -89,6 +117,28 @@
 		</div>
 	</div>
 </div>
+<style type="text/css">
+	
+.vacio{
+border-color: gray;
+}
+
+.error{
+	color: red;
+}
+
+.bien{
+
+	color:green;
+
+}
+.xvacio
+{
+
+	color: black;
+}
+
+</style>
 
 
 {!! Form::close() !!} @endsection @section('tabla')
@@ -99,6 +149,40 @@
 <script type="text/javascript">
 	$(document).ready(function() {
 					$('#tipop').hide();
+
+
+ $("#nombres").addClass("vacio");
+ $("#primerAp").addClass("vacio");
+ $("#segundoAp").addClass("vacio");
+  $("#rfc").addClass("vacio");
+ $("#homo").addClass("vacio");
+ $("#curp").addClass("vacio");
+  $("#telefono").addClass("vacio");
+ $("#motivoEstancia").addClass("vacio");
+ $("#docIdentificacion").addClass("vacio");  	
+$("#numDocIdentificacion").addClass("vacio");
+
+
+ $("#calle").addClass("vacio");
+ $("#numExterno").addClass("vacio");  	
+$("#numInterno").addClass("vacio");
+
+ $("#lugarTrabajo").addClass("vacio");
+ $("#telefonoTrabajo").addClass("vacio");  	
+$("#calle2").addClass("vacio");
+
+
+$("#calle3").addClass("vacio");
+ $("#numExterno3").addClass("vacio");  	
+$("#numInterno3").addClass("vacio");
+$("#correo").addClass("vacio");
+ $("#telefono").addClass("vacio");  	
+$("#fax").addClass("vacio");
+
+
+
+
+
 	});
 	$('input[type=radio][name=esVictima]').change(function() {
 		if (this.value == 0) {
@@ -108,5 +192,16 @@
 		}
 			$('#tipop').show();
 	});
+
+
+
 </script>
+
+
+
 @endpush
+
+
+
+
+
