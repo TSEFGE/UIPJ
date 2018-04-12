5aecd29fb55fd022ecc7b601b96cce43586b90f2
Nain Lobato <wolverinenain@gmail.com>
2018-04-12 13:09:02 -0500

    Generación de documento de citatorio de Investigado

--- app/Http/Controllers/CitatorioController.php
+++ app/Http/Controllers/CitatorioController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use DB;
 use Alert;
 use Carbon\Carbon;
+use App\Http\Controllers\DocxMakerController;
 use App\Models\Citatorio;
 
 class CitatorioController extends Controller
@@ -40,15 +43,72 @@ Citatorio     * @return \Illuminate\Http\Response
     {
         //dd($request->all());
         $fecha = Carbon::parse($request->fecha)->format("Y-d-m H:i:00");
-        //if($request->tipoInvolucrado=="")
+        //if($request->tipoInvolucrado==1)
+        $info = DB::table('extra_denunciado')
+            ->join('variables_persona', 'variables_persona.id', '=', 'extra_denunciado.idVariablesPersona')
+            ->join('domicilio', 'domicilio.id', '=', 'variables_persona.idDomicilio')
+            ->join('cat_municipio', 'cat_municipio.id', '=', 'domicilio.idMunicipio')
+            ->join('cat_estado', 'cat_estado.id', '=', 'cat_municipio.idEstado')
+            ->join('cat_localidad', 'cat_localidad.id', '=', 'domicilio.idLocalidad')
+            ->join('cat_colonia', 'cat_colonia.id', '=', 'domicilio.idColonia')
+            ->join('persona', 'persona.id', '=', 'variables_persona.idPersona')
+            ->join('carpeta', 'carpeta.id', '=', 'variables_persona.idCarpeta' )
+            ->join('unidad', 'unidad.id', '=', 'carpeta.idUnidad')
+            ->join('users', 'users.id', '=', 'carpeta.idFiscal')
+            ->select('persona.nombres as nombresD', 'persona.primerAp as primerApD', 'persona.segundoAp as segundoApD', 'carpeta.id', 'carpeta.numCarpeta', 'carpeta.fechaInicio', 'unidad.direccion', 'unidad.telefono', 'unidad.distrito', 'unidad.municipio', 'users.nombres', 'users.apellidos', 'users.numFiscal', 'domicilio.calle', 'domicilio.numExterno', 'domicilio.numInterno',  'cat_municipio.nombre as municipio2', 'cat_estado.nombre as estado', 'cat_localidad.nombre as localidad', 'cat_colonia.nombre as colonia', 'cat_colonia.codigoPostal as cp')
+            ->where('extra_denunciado.id', '=', $request->idCitado)
+            ->get();
+        //dd($info);
+        $info=$info[0];
+
+        $distritoLetra = DocxMakerController::getDistritoLetra($info->distrito);
+        $fechaHoy = new Carbon();
+        $mesLetra = DocxMakerController::getMesLetra($fechaHoy->month);
+        $fechaCompleta = mb_strtoupper($fechaHoy->day." DE ".$mesLetra." DE ".$fechaHoy->year);
+        $nombreFiscal = mb_strtoupper($info->nombres." ".$info->apellidos);
+        //$diaLetra = mb_strtolower(DocxMakerController::getDiaLetra($fechaHoy->day));
+        //$mesLetra = mb_strtolower($mesLetra);
+        $diaCit = Carbon::parse($request->fecha)->format("d");
+        $mesCit = Carbon::parse($request->fecha)->format("m");
+        $yearCit = Carbon::parse($request->fecha)->format("Y");
+        $mesLetraCit = DocxMakerController::getMesLetra((int)$mesCit);
+        $fechaCompletaCit = mb_strtoupper($diaCit." DE ".$mesLetraCit." DE ".$yearCit);
+        $horaCit = Carbon::parse($request->fecha)->format("H:i");
+
+        $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('templates/CitaInvestigado.docx');
+        $templateProcessor->setValue('distritoLetra', $distritoLetra);
+        $templateProcessor->setValue('municipioUnidadM', mb_strtoupper($info->municipio));
+        $templateProcessor->setValue('motivoCita', $request->motivo);
+        $templateProcessor->setValue('numCarpeta', $info->numCarpeta);
+        $templateProcessor->setValue('numCitatorio', 0);//numCitatorio
+        $templateProcessor->setValue('fechaCompleta', $fechaCompleta);
+        $templateProcessor->setValue('distrito', $info->distrito);
+        $templateProcessor->setValue('numFiscal', $info->numFiscal);
+        $templateProcessor->setValue('numOficio', 0);
+        $templateProcessor->setValue('anio', $fechaHoy->year);
+        $templateProcessor->setValue('nombreDenunciado', $info->nombresD." ".$info->primerApD." ".$info->segundoApD);
+        $templateProcessor->setValue('calleDen', $info->calle);//calleDen
+        $templateProcessor->setValue('coloniaDen', $info->colonia);//coloniaDen
+        $templateProcessor->setValue('municipioDen', $info->municipio);//municipioDen
+        $templateProcessor->setValue('fechaCitatorio', $fechaCompletaCit);//fechaCitatorio
+        $templateProcessor->setValue('horaCitatorio', $horaCit);//horaCitatorio
+        $templateProcessor->setValue('direccion', $info->direccion);
+        $templateProcessor->setValue('telefono', $info->telefono);
+        $templateProcessor->setValue('nombreFiscal', $nombreFiscal);
+        
+        $name='Citatorio'.time().'.docx';
+        $templateProcessor->saveAs(public_path().'\storage\citatorios\\'.$name);
+        //$templateProcessor->saveAs('../storage/oficios/ConstanciaDeHechos'.$info->id.'.docx');
+
         //Generar documento
         $citatorio = new Citatorio($request->all());
         $citatorio->fecha = $fecha;
         $citatorio->intento = 1;
-        $citatorio->documento = "xd.jpg";
+        $citatorio->documento = $name;
         $citatorio->save();
         //Retornar documento
         Alert::success('Citatorio registrado con éxito', 'Hecho')->persistent("Aceptar");
+        return response()->download(public_path().'\storage\citatorios\\'.$name);
         //return redirect()->route('carpeta', $request->idCarpeta);
         return redirect()->route('citatorio', ['idCarpeta'=>$request->idCarpeta,'idCitado'=>$request->idCitado, 'tipoInvolucrado'=>$request->tipo]);
     
