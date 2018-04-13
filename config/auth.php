b96a0e077f03e74d04ac61e6a452d7bd325b0707
ADFGE\kguzman <isckg93@gmail.com>
2018-04-13 12:25:35 -0500

    no message

--- config/auth.php
+++ config/auth.php
@@ -66,7 +66,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',//'adldap',
+            'driver' => 'adldap',//'adldap',
             'model' => App\User::class,
         ],
 
