18b74beb89f283c84f4e53b259480eedf0673e8e
ADFGE\kguzman <isckg93@gmail.com>
2018-04-13 11:08:28 -0500

    Edición Tabla Citatorio

--- resources/views/tables/citatorios.blade.php
+++ resources/views/tables/citatorios.blade.php
@@ -1,10 +1,11 @@
 <div class="table">
     <table class="table table-striped">
-        <thead>
+        <thead align="center">
             <th>Tipo</th>
+            <th>Estado</th>
             <th>Fecha</th>
             <th>Hora</th>
-            <th>Estado</th>
+            
             <th>Número</th>
             <th>Generar Documento</th>  
         </thead>
@@ -13,7 +14,7 @@
                 <tr><td colspan="6" class="text-center">SIN RESGISTROS</td></tr>
             @else
                 @foreach($citatorios as $citatorio)
-                    <tr>
+                    <tr align="center">
                         @if($citatorio->tipo == 1)
                             <td>INVESTIGADO</td>
                         @else
@@ -31,7 +32,7 @@
                         <td>{{ $citatorio->fecha }}</td>
                         <td>{{ $citatorio->hora }}</td>
                         <td>PRIMERA</td>
-                        <td>PRIMERA</td>
+                     
                         <td align="center"><a href="{{ asset('storage/citatorios/'.$citatorio->documento) }}" class="btn btn-info" target="_blank">Ver documento</a></td>
                     </tr>
                 @endforeach
