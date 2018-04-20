c6ab7d945664092bc19ec3fb23debd5a3b36176c
Nain Lobato <wolverinenain@gmail.com>
2018-04-20 12:29:26 -0500

    Cambio en etiqueta, cambio en semilla.

--- resources/views/tables/acusaciones.blade.php
+++ resources/views/tables/acusaciones.blade.php
@@ -4,9 +4,7 @@
             <th>Nombre denunciante</th>
             <th>Delito</th>
             <th>Nombre denunciado</th>
-            <th>Formato de denuncia</th>
-        
-           
+            <th>Formato de inicio</th>
         </thead>
         <tbody>
             @if(count($acusaciones)==0)
@@ -17,8 +15,6 @@
                         <td>{{ $acusacion->nombres." ".$acusacion->primerAp." ".$acusacion->segundoAp }}</td>
                         <td>{{ $acusacion->delito }}</td>
                         <td>{{ $acusacion->nombres2." ".$acusacion->primerAp2." ".$acusacion->segundoAp2 }}</td>
-                      
-
                         <td align="center"><a href="{{route('formato.denuncia', $acusacion->id)}}"> <i class="fa fa-cloud-download" style="font-size:24px;color:grey"></i></a></td> 
                     </tr>
                 @endforeach
