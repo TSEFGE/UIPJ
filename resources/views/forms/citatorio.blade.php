829db2a92652b436bf386facc9103922e1acd798
ADFGE\afernandezl <ing.a.landero@gmail.com>
2018-04-20 14:07:13 -0500

    boton regresar a carpeta

--- resources/views/forms/citatorio.blade.php
+++ resources/views/forms/citatorio.blade.php
@@ -17,8 +17,9 @@
 			{!! Form::hidden('status', 1) !!}
 		</div>
 		<div class="col">
-			<div class="text-right">
+			<div class="text-right">				
 				{!! Form::button('<i class="fa fa-eraser" aria-hidden="true"></i>', array ('class' => 'btn btn-primary borrar ', 'id' => 'btn-reset')) !!}
+				<a id="regresocarpeta" href="{{ route('carpeta', $idCarpeta) }}" class="btn btn-primary borrar"><i class="fa fa-folder-open" aria-hidden="true"></i></a>
 				{!! Form::submit('Guardar', ['class' => 'btn btn-primary', 'id' => 'btn-submit']) !!}
 			</div>
 		</div>
