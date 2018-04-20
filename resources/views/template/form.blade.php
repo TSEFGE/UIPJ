31086398e69047b8824dba22b2ed4890903b013c
Nain Lobato <wolverinenain@gmail.com>
2018-04-20 13:06:22 -0500

    Exclusiín sisyphus.

--- resources/views/template/form.blade.php
+++ resources/views/template/form.blade.php
@@ -34,6 +34,7 @@
     <script src="{{ asset('plugins/jquery-validator/js/jquery.form-validator.min.js')}}" ></script>
     <script src="{{ asset('plugins/jquery-ui/js/jquery-ui.js')}}" ></script>
     <script src="{{ asset('plugins/sisyphus/js/sisyphus.js')}}" ></script>
+    <script src="{{ asset('js/selects/sisy.js') }}"></script>
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
