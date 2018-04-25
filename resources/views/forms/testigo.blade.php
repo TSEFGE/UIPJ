16ad22e211730ca00c9062136563b2237ad1287a
ADFGE\afernandezl <ing.a.landero@gmail.com>
2018-04-25 13:44:11 -0500

    JS para validar datos en pestaña testigo.

--- resources/views/forms/testigo.blade.php
+++ resources/views/forms/testigo.blade.php
@@ -100,7 +100,7 @@
 	<script src="{{ asset('plugins/moment/js/moment.min.js') }}"></script>
     <script src="{{ asset('plugins/moment/locales/es.js') }}"></script>
 	<script src="{{ asset('plugins/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-	<script src="{{ asset('js/persona.js') }}"></script>
+	<script src="{{ asset('js/testigo.js') }}"></script>
     <script src="{{ asset('js/selects/async.js') }}"></script>
     <script src="{{ asset('js/selects/origen.js') }}"></script>
     <script src="{{ asset('js/selects/domicilio.js') }}"></script>
