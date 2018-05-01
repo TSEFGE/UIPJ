6d972782f55eab427ce99bf4b038e7711400ede9
ADFGE\garenas <uncubus.agaa@gmail.com>
2018-05-01 12:20:13 -0500

    vista familiar edit

---
+++ resources/views/edit-forms/familiar.blade.php
@@ -0,0 +1,34 @@
+@extends('template.form')
+
+@section('title', 'Agregar Familiar')
+
+@section('contenido')
+{!! Form::open(['route' => 'store.familiar', 'method' => 'POST'])  !!}
+{{ csrf_field() }}
+	<div class="card-header">
+		<div class="row">
+			<div class="col">
+				<div class="text-left">
+					{{--Aqui van radios, etc --}}
+				</div>
+			</div>
+			<div class="col">	
+				<div class="text-right">
+						@include('forms.buttons')
+				</div>
+			</div>
+		</div>
+	</div>
+	@include('forms.errores')
+	<div class=" card-body boxone">
+		<div class="row no-gutters">
+			<div class="col-12">
+				<div class="boxtwo">
+					<h6>Datos del familiar</h6>
+					@include('fields.familiar')
+				</div>
+			</div>
+		</div>
+	</div>
+	{!! Form::close() !!}
+@endsection
