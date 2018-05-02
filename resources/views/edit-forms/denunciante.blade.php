fb57b0ab63ed0a79d8381d48a909c19bc4bd8601
ADFGE\garenas <uncubus.agaa@gmail.com>
2018-05-02 10:25:56 -0500

    Vista denunciante correctamente visualizada

--- resources/views/edit-forms/denunciante.blade.php
+++ resources/views/edit-forms/denunciante.blade.php
@@ -70,11 +70,15 @@
 				@if ( ($personales->esEmpresa) == 1)
 					@include('edit-fields.personales-moral')
 					@include('fields.extra-denunciante')
-					@endif
-			@else if ( ($personales->esEmpresa) == 0)
+				@endif
+			@endif
+			@if (isset ($personales) )
+				@if ( ($personales->esEmpresa) == 0)
 				@include('edit-fields.personales-fisica')							
 				@include('fields.extra-denunciante')
-			@endif		
+				@endif
+			@endif
+				
 			{{--@include('edit-fields.personales-fisica')							
 				@include('fields.extra-denunciante')--}}	
 			</div>
