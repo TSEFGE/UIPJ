c8abbc495d4ef0d5b7aea697785f209a22444432
Donaldo Lopez <donaldolog@gmail.com>
2018-05-03 08:04:34 -0500

    Metodo update tstigo

--- app/Http/Controllers/TestigoController.php
+++ app/Http/Controllers/TestigoController.php
@@ -262,18 +262,21 @@ class TestigoController extends Controller
                         ->join('cat_municipio', 'cat_municipio.id', '=', 'domicilio.idMunicipio')
                         ->join('cat_colonia', 'cat_colonia.id', '=', 'domicilio.idColonia')
                         ->select('domicilio.*', 'domicilio.id as idDomicilio', 'cat_colonia.codigoPostal')
+                        ->where('domicilio.id', '=', $personales->idDomicilio)
                         ->get()->first();
             $direccionTrab=DB::table('variables_persona', 'variables_persona.idPersona', '=', $personales->idPersona)
                         ->join('domicilio', 'domicilio.id', '=', 'variables_persona.idDomicilioTrabajo')
                         ->join('cat_municipio', 'cat_municipio.id', '=', 'domicilio.idMunicipio')
                         ->join('cat_colonia', 'cat_colonia.id', '=', 'domicilio.idColonia')
                         ->select('domicilio.*', 'domicilio.id as idDomicilio', 'cat_colonia.codigoPostal')
+                        ->where('variables_persona.idPersona', '=', $personales->idPersona)
                         ->get()->first();
             $direccionNotif=DB::table('notificacion', 'notificacion.id', '=', $personales->idNotificacion)
                         ->join('domicilio', 'domicilio.id', '=', 'notificacion.idDomicilio')
                         ->join('cat_municipio', 'cat_municipio.id', '=', 'domicilio.idMunicipio')
                         ->join('cat_colonia', 'cat_colonia.id', '=', 'domicilio.idColonia')
                         ->select('domicilio.*', 'domicilio.id as idDomicilio', 'cat_colonia.codigoPostal')
+                        ->where('notificacion.id', '=', $personales->idNotificacion)
                         ->get()->first();
 
             $carpeta=Carpeta::find($idCarpeta);
@@ -315,6 +318,183 @@ class TestigoController extends Controller
 
     public function update(Request $request, $id)
     {
-        return redirect()->route('home');
+        $persona = Persona::where('curp', $request->curp)->get();
+        if ($persona->isNotEmpty()) {
+            Alert::error('Ya existe una persona registrada con ese CURP.', 'Error')->persistent("Aceptar");
+            return back()->withInput();
+        } else {
+            $persona->nombres         = $request->nombres;
+            $persona->primerAp        = $request->primerAp;
+            $persona->segundoAp       = $request->segundoAp;
+            $fechaAux                 = $request->fechaNacimiento;
+            $fechaNacimiento          = date("Y-m-d", strtotime($fechaAux));
+            $persona->fechaNacimiento = $fechaNacimiento;
+
+            $persona->rfc  = $request->rfc . $request->homo;
+            $persona->curp = $request->curp;
+            if (!is_null($request->sexo)) {
+                $persona->sexo = $request->sexo;
+            }
+            if (!is_null($request->idNacionalidad)) {
+                $persona->idNacionalidad = $request->idNacionalidad;
+            }
+            if (!is_null($request->idEtnia)) {
+                $persona->idEtnia = $request->idEtnia;
+            }
+            if (!is_null($request->idLengua)) {
+                $persona->idLengua = $request->idLengua;
+            }
+            if (!is_null($request->idMunicipioOrigen)) {
+                $persona->idMunicipioOrigen = $request->idMunicipioOrigen;
+            }
+            $persona->save();
+            if ($request->rfcAux != $request->rfc . $request->homo) {
+                Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'persona', 'accion' => 'update', 'descripcion' => 'Se ha registrado y actualizado un RFC diferente al generado por el sistema para una persona física de tipo testigo.', 'idFilaAccion' => $persona->id]);
+            }
+            Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'persona', 'accion' => 'update', 'descripcion' => 'Se ha actualizado  persona física de tipo testigo.', 'idFilaAccion' => $persona->id]);
+            $idPersona = $persona->id;
+
+            $domicilio = Domicilio::where('id', $request->idDomicilio)->get();
+            if (!is_null($request->idMunicipio)) {
+                $domicilio->idMunicipio = $request->idMunicipio;
+            }
+            if (!is_null($request->idLocalidad)) {
+                $domicilio->idLocalidad = $request->idLocalidad;
+            }
+            if (!is_null($request->idColonia)) {
+                $domicilio->idColonia = $request->idColonia;
+            }
+            if (!is_null($request->calle)) {
+                $domicilio->calle = $request->calle;
+            }
+            if (!is_null($request->numExterno)) {
+                $domicilio->numExterno = $request->numExterno;
+            }
+            if (!is_null($request->numInterno)) {
+                $domicilio->numInterno = $request->numInterno;
+            }
+            $domicilio->save();
+            Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'domicilio', 'accion' => 'update', 'descripcion' => 'Se ha actualizado domicilio para persona física de tipo testigo.', 'idFilaAccion' => $domicilio->id]);
+            $idD1 = $domicilio->id;
+
+            $domicilio2 = Domicilio::where('id', $request->idDomicilioTrabajo)->get();
+            if (!is_null($request->idMunicipio2)) {
+                $domicilio2->idMunicipio = $request->idMunicipio2;
+            }
+            if (!is_null($request->idLocalidad2)) {
+                $domicilio2->idLocalidad = $request->idLocalidad2;
+            }
+            if (!is_null($request->idColonia2)) {
+                $domicilio2->idColonia = $request->idColonia2;
+            }
+            if (!is_null($request->calle2)) {
+                $domicilio2->calle = $request->calle2;
+            }
+            if (!is_null($request->numExterno2)) {
+                $domicilio2->numExterno = $request->numExterno2;
+            }
+            if (!is_null($request->numInterno2)) {
+                $domicilio2->numInterno = $request->numInterno2;
+            }
+            $domicilio2->save();
+            Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'domicilio', 'accion' => 'update', 'descripcion' => 'Se ha actualizado domicilio de trabajo para persona física de tipo testigo.', 'idFilaAccion' => $domicilio2->id]);
+            $idD2 = $domicilio2->id;
+
+            $domicilio3 =Domicilio::where('id', $request->idDomicilioNotif);
+            if (!is_null($request->idMunicipio3)) {
+                $domicilio3->idMunicipio = $request->idMunicipio3;
+            }
+            if (!is_null($request->idLocalidad3)) {
+                $domicilio3->idLocalidad = $request->idLocalidad3;
+            }
+            if (!is_null($request->idColonia3)) {
+                $domicilio3->idColonia = $request->idColonia3;
+            }
+            if (!is_null($request->calle3)) {
+                $domicilio3->calle = $request->calle3;
+            }
+            if (!is_null($request->numExterno3)) {
+                $domicilio3->numExterno = $request->numExterno3;
+            }
+            if (!is_null($request->numInterno3)) {
+                $domicilio3->numInterno = $request->numInterno3;
+            }
+            $domicilio3->save();
+            Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'domicilio', 'accion' => 'update', 'descripcion' => 'Se ha actualizado domicilio para notificaciones para persona física.', 'idFilaAccion' => $domicilio3->id]);
+            $idD3 = $domicilio3->id;
+
+            $notificacion              = Notificacion::where('id', $request->idNotificacion)->get();
+            $notificacion->idDomicilio = $idD3;
+            $notificacion->correo      = $request->correo;
+            $notificacion->telefono    = $request->telefono;
+            $notificacion->fax         = $request->fax;
+            $notificacion->save();
+            Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'notificacion', 'accion' => 'update', 'descripcion' => 'Se ha actualizado  notificación para persona física de tipo testigo.', 'idFilaAccion' => $notificacion->id]);
+            $idNotificacion = $notificacion->id;
+
+            $VariablesPersona            = VariablesPersona::where('id', $request->idNotificacion)->get();
+            $VariablesPersona->idCarpeta = $request->idCarpeta;
+            $VariablesPersona->idPersona = $idPersona;
+            $VariablesPersona->edad      = $request->edad;
+            if (!is_null($request->telefono)) {
+                $VariablesPersona->telefono = $request->telefono;
+            }
+            if (!is_null($request->motivoEstancia)) {
+                $VariablesPersona->motivoEstancia = $request->motivoEstancia;
+            }
+            if (!is_null($request->idOcupacion)) {
+                $VariablesPersona->idOcupacion = $request->idOcupacion;
+            }
+            if (!is_null($request->idEstadoCivil)) {
+                $VariablesPersona->idEstadoCivil = $request->idEstadoCivil;
+            }
+            if (!is_null($request->idEscolaridad)) {
+                $VariablesPersona->idEscolaridad = $request->idEscolaridad;
+            }
+            if (!is_null($request->idReligion)) {
+                $VariablesPersona->idReligion = $request->idReligion;
+            }
+            $VariablesPersona->idDomicilio = $idD1;
+            if (!is_null($request->docIdentificacion)) {
+                $VariablesPersona->docIdentificacion = $request->docIdentificacion;
+            }
+            if (!is_null($request->numDocIdentificacion)) {
+                $VariablesPersona->numDocIdentificacion = $request->numDocIdentificacion;
+            }
+            if (!is_null($request->lugarTrabajo)) {
+                $VariablesPersona->lugarTrabajo = $request->lugarTrabajo;
+            }
+            $VariablesPersona->idDomicilioTrabajo = $idD2;
+            if (!is_null($request->telefonoTrabajo)) {
+                $VariablesPersona->telefonoTrabajo = $request->telefonoTrabajo;
+            }
+            $VariablesPersona->representanteLegal = "NO APLICA";
+            $VariablesPersona->save();
+            Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'variables_persona', 'accion' => 'update', 'descripcion' => 'Se ha actualizado variables persona de persona física de tipo testigo.', 'idFilaAccion' => $VariablesPersona->id]);
+            $idVariablesPersona = $VariablesPersona->id;
+
+            $ExtraTestigo                     = ExtraTestigo::where('id', $request->$idExtraTestigo)->get();
+            if ($request->conoceAlDenunciado === 1) {
+                $ExtraTestigo->conoceAlDenunciado = 1;
+            }
+
+            $ExtraTestigo->save();
+            Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'extra_testigo', 'accion' => 'update', 'descripcion' => 'Se ha actualizado extra testigo de persona física.', 'idFilaAccion' => $ExtraTestigo->id]);
+
+            // $narracion                = new Narracion();
+            // $narracion->idInvolucrado = $ExtraTestigo->id;
+            // $narracion->idCarpeta     = $request->idCarpeta;
+            // //dd($request);
+            // $narracion->narracion       = $request->narracionUno;
+            // $narracion->tipoInvolucrado = 4;
+            // $narracion->archivo         = null;
+            // $narracion->save();
+
+            //Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'narracion', 'accion' => 'update', 'descripcion' => 'Se ha actualizado  narracion de persona física de tipo testigo.', 'idFilaAccion' => $narracion->id]);
+
+            Alert::success('Testigo actualizado con éxito', 'Hecho')->persistent("Aceptar");
+            //return redirect()->route('carpeta', $request->idCarpeta);
+            return redirect()->route('new.testigo', $request->idCarpeta);
+        }
     }
 }
