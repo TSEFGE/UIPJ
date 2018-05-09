cad55a96d3bbe55f7bd46b7fde819f8d8e24511a
Nain Lobato <wolverinenain@gmail.com>
2018-05-09 09:46:55 -0500

    Libro de oficios 1/2

--- app/Http/Controllers/LibroOficiosController.php
+++ app/Http/Controllers/LibroOficiosController.php
@@ -14,12 +14,20 @@ class LibroOficiosController extends Controller
     }
 
     public function apiLibro(){
-      $oficios = DB::table('diligencias_sp')
+      	$oficios = DB::table('diligencias_sp')
       		->join('acusacion','acusacion.id','=','diligencias_sp.idAcusacion')
       		->join('carpeta','carpeta.id','=','acusacion.idCarpeta')
             ->join('users','users.id','=','carpeta.idFiscal')
-            ->select('diligencias_sp.numOficio', 'carpeta.numCarpeta', 'diligencias_sp.created_at', 'diligencias_sp.status', 'diligencias_sp.oficio', 'diligencias_sp.created_at')
+            ->select('diligencias_sp.numOficio', 'carpeta.numCarpeta', 'diligencias_sp.created_at', 'diligencias_sp.status', 'diligencias_sp.oficio', 'diligencias_sp.created_at', DB::raw('CONCAT("PERICIALES") AS extra'))
             ->where('users.id','=', Auth::user()->id);
+
+        /*$oficios = DB::table('diligencias_pm')
+      		->join('acusacion','acusacion.id','=','diligencias_sp.idAcusacion')
+      		->join('carpeta','carpeta.id','=','acusacion.idCarpeta')
+            ->join('users','users.id','=','carpeta.idFiscal')
+            ->select('diligencias_sp.numOficio', 'carpeta.numCarpeta', 'diligencias_sp.created_at', 'diligencias_sp.status', 'diligencias_sp.oficio', 'diligencias_sp.created_at', DB::raw('CONCAT("PERICIALES") AS extra'))
+            ->where('users.id','=', Auth::user()->id)
+            ->->union($oficiossp);*/
       return Datatables::of($oficios)->make(true);
     }
 }
