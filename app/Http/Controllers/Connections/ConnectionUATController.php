d04cb067371cb85103153f7c25c8dfb6011c7319
Donaldo Lopez <donaldolog@gmail.com>
2018-05-10 09:38:29 -0500

    uat controller / web

---
+++ app/Http/Controllers/Connections/ConnectionUATController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Connections;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ConnectionUATController extends Controller
+{
+    //
+}
