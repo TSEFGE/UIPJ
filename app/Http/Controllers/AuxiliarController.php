775724ed6dc60bffcacd8e9c90637adb81c821cb
ADFGE\kguzman <isckg93@gmail.com>
2018-05-15 11:00:12 -0500

    método store de auxiliar, avance, faltan pruebas.

--- app/Http/Controllers/AuxiliarController.php
+++ app/Http/Controllers/AuxiliarController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 class AuxiliarController extends Controller
 {
     public function showForm()
@@ -12,5 +14,17 @@ class AuxiliarController extends Controller
     public function storeAuxiliar(StoreAuxiliar $request)
     {
 
+        $auxiliar               = new Auxiliar();
+        $auxiliar->idFiscal     = Auth::user()->id;
+        $auxiliar->nombres      = $request->nombres;
+        $auxiliar->primerAp     = $request->primerAp;
+        $auxiliar->segundoAp    = $request->segundoAp;
+        $auxiliar->email        = $request->email;
+        $auxiliar->telefono     = $request->telefono;
+        $auxiliar->password     = $request->password;
+        $auxiliar->tokenSession = null;
+        $auxiliar->save();
+
+        Bitacora::create(['idUsuario' => Auth::user()->id, 'tabla' => 'auxiliar', 'accion' => 'create', 'descripcion' => 'Se ha dado de alta el auxiliar' . $auxiliar->nombres . ' ' . $auxiliar->primerAp . ' ' . $auxiliar->segundoAp . '.', 'idFilaAccion' => $auxiliar->id]);
     }
 }
